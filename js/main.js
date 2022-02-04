aa808ad1b45a1b915a8ae63609f6208abb65474c
csslick <digitone@naver.com>
2022-02-04 16:28:21 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+// 메인이미지 변경 함수
+function changeVisual(num) {
+  // 버튼스타일 및 이미지 초기화
+  $('.tab-group a').removeClass('on');
+  $('.slider > img').removeClass('on');
+
+  if(num == 1) {
+    // 해당 이미지 켜주세요
+    $('.tab-group a').eq(num - 1).addClass('on');
+  } else if(num == 2) {
+    // 해당 이미지 켜주세요
+    $('.tab-group a').eq(num - 1).addClass('on');
+  } else if(num == 3) {
+    // 해당 이미지 켜주세요
+    $('.tab-group a').eq(num - 1).addClass('on');
+  }   
+}
+/* 
+function changeVisual(num) {
+  $('.tab-group a').removeClass('on');
+
+  if(num == 1) {
+    $('#main-banner img').attr('src', './images/main-banner1.jpg');
+    $('.tab-group a').eq(num - 1).addClass('on');
+  } else if(num == 2) {
+    $('#main-banner img').attr('src', './images/main-banner2.jpg');
+    $('.tab-group a').eq(num - 1).addClass('on');
+  } else if(num == 3) {
+    $('#main-banner img').attr('src', './images/main-banner3.jpg');
+    $('.tab-group a').eq(num - 1).addClass('on');
+  } 
+} 
+*/
